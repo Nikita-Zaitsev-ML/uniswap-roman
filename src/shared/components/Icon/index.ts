e74b16c82d11a8b27b3bc7264b52570b787c6044
Nikita Zaitsev <nikita.zaitsev@metalamp.io>
2022-05-31 09:59:12 +0300

    refac(delete unused components)

--- src/shared/components/Icon/index.ts
+++ src/shared/components/Icon/index.ts
@@ -1,33 +1,5 @@
 import { Add } from './Add/Add';
 import { ArrowDownward } from './ArrowDownward/ArrowDownward';
-import { ArrowRightIcon } from './ArrowRightIcon/ArrowRightIcon';
-import { Check } from './Check/Check';
-import { CloudDoneIcon } from './CloudDoneIcon/CloudDoneIcon';
-import { CloudDownloadIcon } from './CloudDownloadIcon/CloudDownloadIcon';
-import { CloudErrorIcon } from './CloudErrorIcon/CloudErrorIcon';
-import { ExpandMore } from './ExpandMore/ExpandMore';
-import { LabelIcon } from './LabelIcon/LabelIcon';
-import { MenuIcon } from './MenuIcon/MenuIcon';
-import { PlayCircle } from './PlayCircle/PlayCircle';
-import { SadEmoticon } from './SadEmoticon/SadEmoticon';
-import { Stop } from './Stop/Stop';
 import { SVGIcon } from './SVGIcon/SVGIcon';
-import { WarningIcon } from './WarningIcon/WarningIcon';
 
-export {
-  Add,
-  ArrowDownward,
-  ArrowRightIcon,
-  Check,
-  CloudDoneIcon,
-  CloudDownloadIcon,
-  CloudErrorIcon,
-  ExpandMore,
-  LabelIcon,
-  MenuIcon,
-  PlayCircle,
-  SadEmoticon,
-  Stop,
-  SVGIcon,
-  WarningIcon,
-};
+export { Add, ArrowDownward, SVGIcon };
--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,89 +1,43 @@
-import { Avatar } from './Avatar/Avatar';
 import { Autocomplete } from './Autocomplete/Autocomplete';
+import { Avatar } from './Avatar/Avatar';
 import { Box } from './Box/Box';
 import { Breadcrumbs } from './Breadcrumbs/Breadcrumbs';
+import { Button } from './Button/Button';
+import { Card } from './Card/Card';
 import { Container } from './Container/Container';
-import { Header } from './Header/Header';
-import {
-  Add,
-  ArrowDownward,
-  ArrowRightIcon,
-  CloudDoneIcon,
-  CloudErrorIcon,
-  CloudDownloadIcon,
-  MenuIcon,
-  PlayCircle,
-  Stop,
-  WarningIcon,
-  ExpandMore,
-  LabelIcon,
-} from './Icon';
-import { IconButton } from './IconButton/IconButton';
-import { LazyContainer } from './LazyContainer/LazyContainer';
-import { Img } from './Img/Img';
-import { Link } from './Link/Link';
-import { Menu } from './Menu/Menu';
-import { PageHead } from './PageHead/PageHead';
-import { Palette } from './Palette/Palette';
-import { TextField, InputAdornment } from './TextField/TextField';
-import { Typography } from './Typography/Typography';
-import { MediaControls } from './MediaControls/MediaControls';
 import { Grid } from './Grid/Grid';
-import { Popper } from './Popper/Popper';
-import { Popover } from './Popover/Popover';
+import { Header } from './Header/Header';
+import { SVGIcon, Add, ArrowDownward } from './Icon';
+import { Img } from './Img/Img';
+import { LazyContainer } from './LazyContainer/LazyContainer';
+import { Link } from './Link/Link';
+import { PageHead } from './PageHead/PageHead';
 import { CircularProgress, LinearProgress } from './Progress';
 import { Slider } from './Slider/Slider';
-import { List } from './List/List';
-import { ListItemButton } from './ListItemButton/ListItemButton';
-import { Button } from './Button/Button';
-import { Alert } from './Alert/Alert';
-import { SVGIcon } from './Icon/SVGIcon/SVGIcon';
-import { Accordion } from './Accordion/Accordion';
-import { Card } from './Card/Card';
-import { Tabs } from './Tabs/Tabs';
+import { TextField, InputAdornment } from './TextField/TextField';
+import { Typography } from './Typography/Typography';
 
 export {
-  Avatar,
   Autocomplete,
+  Avatar,
   Box,
   Breadcrumbs,
+  Button,
+  Card,
   Container,
+  Grid,
   Header,
+  SVGIcon,
   Add,
   ArrowDownward,
-  ArrowRightIcon,
-  CloudDoneIcon,
-  CloudErrorIcon,
-  CloudDownloadIcon,
-  MenuIcon,
-  PlayCircle,
-  Stop,
-  WarningIcon,
-  ExpandMore,
-  LabelIcon,
-  IconButton,
-  LazyContainer,
   Img,
+  LazyContainer,
   Link,
-  Menu,
   PageHead,
-  Palette,
-  TextField,
-  InputAdornment,
-  Typography,
-  MediaControls,
-  Grid,
-  Popper,
-  Popover,
   CircularProgress,
   LinearProgress,
   Slider,
-  List,
-  ListItemButton,
-  Button,
-  Alert,
-  SVGIcon,
-  Accordion,
-  Card,
-  Tabs,
+  TextField,
+  InputAdornment,
+  Typography,
 };
